ceeed37ba3724deb11ed2ca3130ac8c497883b12
samer al-khatib <samer_mail_2006@yahoo.com>
2017-02-23 20:50:47 +0300

    Add Singleton pattern.

---
+++ Src/SingletonPattern/LoadBalancer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingletonPattern
+{
+    public class LoadBalancer
+    {
+        private static volatile LoadBalancer _instance;
+        private List<string> Servers { get; set; }
+        protected LoadBalancer()
+        {
+            Servers = new List<string> { "Serve 1", "Serve 2", "Serve 3", "Serve 4" };
+        }
+
+        public static LoadBalancer GetInstance()
+        {
+            return _instance ?? (_instance = new LoadBalancer());
+        }
+
+        public string Server()
+        {
+            Random r = new Random();
+            int random = r.Next(Servers.Count);
+            return Servers[random];
+
+        }
+    }
+}
---
+++ Src/SingletonPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(LoadBalancer.GetInstance().Server());
+            }
+        }
+    }
+}
