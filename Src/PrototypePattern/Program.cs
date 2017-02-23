293b68847d8434f970c71b3235d6ab1d36f4bd24
samer al-khatib <samer_mail_2006@yahoo.com>
2017-02-23 09:36:34 +0300

    Add Prototype Pattern.

---
+++ Src/PrototypePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorPrototype c = new ColorPrototype { Value = "#000000" };
+            var copy = c.Clone() as ColorPrototype;
+            c.Value = "#ffffff";
+            Console.WriteLine($"Orginal : {c.Value}");
+            Console.WriteLine($"Copy : {copy.Value}");
+
+
+
+        }
+    }
+}
---
+++ Src/PrototypePattern/Prototype.cs
@@ -0,0 +1,18 @@
+﻿namespace PrototypePattern
+{
+    public abstract class Prototype
+    {
+        public abstract Prototype Clone();
+    }
+
+    public class ColorPrototype : Prototype
+    {
+        public string Value { get; set; }
+
+        public override Prototype Clone()
+        {
+            return (Prototype)MemberwiseClone();
+        }
+    }
+
+}
